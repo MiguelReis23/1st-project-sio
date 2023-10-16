0dace7876790d7f314c8d0f859bf0a81917236f3
hramos25 <rodrigomiguel471@gmail.com>
2023-10-16 22:08:14 +0100

    recent

--- app_sec/database.py
+++ app_sec/database.py
@@ -1,7 +1,12 @@
 import sqlite3
+import os
 
 # Connect to the database (creates a new database if it doesn't exist)
 conn = sqlite3.connect('online_shop.db')
+DB_STRING = "app.db"
+
+db_directory = os.path.dirname(os.path.abspath(__file__))
+db_path = os.path.join(db_directory, DB_STRING)
 
 # Create a cursor object to execute SQL commands
 c = conn.cursor()
@@ -16,18 +21,14 @@ c.execute('''CREATE TABLE IF NOT EXISTS products
               stock INTEGER)''')
 
 # Create a table for customers
-c.execute('''CREATE TABLE IF NOT EXISTS customers
-             (id INTEGER PRIMARY KEY,
-              name TEXT NOT NULL,
-              email TEXT)''')
-
-# Create a table for user login information
-c.execute('''CREATE TABLE IF NOT EXISTS users
-             (id INTEGER PRIMARY KEY,
-              username TEXT,
-              password TEXT,
-              permission TEXT,
-              FOREIGN KEY (username) REFERENCES customers(name))''')
+c.execute('''CREATE TABLE IF NOT EXISTS users(
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            first_name VARCHAR(50) NOT NULL DEFAULT '',
+            last_name VARCHAR(50) NOT NULL DEFAULT '', 
+            phone_number CHAR(9) NOT NULL DEFAULT '',
+            username TEXT NOT NULL UNIQUE,
+            email TEXT NOT NULL UNIQUE,
+            password TEXT NOT NULL)''')
 
 # Create a table for orders
 c.execute('''CREATE TABLE IF NOT EXISTS orders
@@ -57,6 +58,26 @@ c.execute('''CREATE TABLE IF NOT EXISTS reviews
               FOREIGN KEY (product_id) REFERENCES products(id),
               FOREIGN KEY (customer_id) REFERENCES customers(name))''')
 
+
+def show_database():
+    users_table = " SELECT * FROM users; "
+
+    with sqlite3.connect(db_path) as conn:
+        cursor = conn.cursor()
+        cursor.execute(users_table)
+        for row in cursor.fetchall():
+            print(row)
+
+    with sqlite3.connect(db_path) as conn:
+        cursor = conn.cursor()
+        for row in cursor.fetchall():
+            print(row)
+
+def clean_database():
+    users_table = " DROP TABLE IF EXISTS users;"
+
+    with sqlite3.connect(db_path) as conn:
+        conn.execute(users_table)
 
 # Save changes and close the connection
 conn.commit()
