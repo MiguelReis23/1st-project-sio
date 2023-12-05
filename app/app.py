918af38a8b0ff99d1b5644a8c52fe9c8fbc05bd3
hramos25 <rodrigomiguel471@gmail.com>
2023-12-05 02:48:44 +0000

    dasdasd

--- app/app.py
+++ app/app.py
@@ -3,16 +3,3 @@ import sqlite3
 
 
 from flask import Flask, render_template, url_for, request, redirect, flash, jsonify
-
-app = Flask(__name__)
-
-@app.route('/')
-def index():
-    return render_template('base.html',title='Test')
-
-@app.route('/checkout')
-def checkout():
-    return render_template('checkout.html',title='Checkout')
-
-if __name__ == '__main__':
-    app.run(port=8080)
